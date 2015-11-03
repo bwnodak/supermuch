e3c18405640676cd3e0597813f4e7543c4bfb478
Brian Nelson <yonbeastie@gmail.com>
2015-11-03 11:48:11 -0600

    Add tests

---
+++ test/test.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+var supermuch = require('../');
+
+describe('Public functions', function() {
+  describe('#add()', function () {
+    it('should return the sum of two stringified integers', function () {
+      assert.equal('0', supermuch.add('0', '0'));
+      assert.equal('2', supermuch.add('1', '1'));
+
+      assert.equal('0', supermuch.add('0', '0', 10));
+      assert.equal('2', supermuch.add('1', '1', 10));
+
+      assert.equal('10', supermuch.add('1', '1', 2));
+      assert.equal('100', supermuch.add('99', '1'));
+      //assert.equal('1000', supermuch.add('zzz', '1'));
+      assert.equal('1zzy', supermuch.add('zzz', 'zzz'));
+    });
+  });
+});
